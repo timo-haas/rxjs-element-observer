bcad098866d6c0cfb03c69f5096faeff9cb70207
timo-haas <44342560+timo-haas@users.noreply.github.com>
2020-10-15 12:05:26 +0200

    chore: build configs (#6)

--- src/functions/fromMutation.ts
+++ src/functions/fromMutation.ts
@@ -1,22 +1,22 @@
-import { EMPTY, Observable } from "rxjs";
+import { EMPTY, Observable } from 'rxjs';
 
 export function fromMutation(
-  domElement: Element
+    domElement: Element,
 ): Observable<MutationRecord[]> {
-  const globalThat = globalThis || window || self;
-  if (!globalThat) {
-    return EMPTY;
-  }
-  if (typeof globalThat.MutationObserver !== "function") {
-    return EMPTY;
-  }
-  return new Observable<MutationRecord[]>((subscriber) => {
-    const elementObserver = new MutationObserver((entries) => {
-      subscriber.next(entries);
+    const globalThat = globalThis || window || self;
+    if (!globalThat) {
+        return EMPTY;
+    }
+    if (typeof globalThat.MutationObserver !== 'function') {
+        return EMPTY;
+    }
+    return new Observable<MutationRecord[]>((subscriber) => {
+        const elementObserver = new MutationObserver((entries) => {
+            subscriber.next(entries);
+        });
+        elementObserver.observe(domElement);
+        return () => {
+            elementObserver.disconnect();
+        };
     });
-    elementObserver.observe(domElement);
-    return () => {
-      elementObserver.disconnect();
-    };
-  });
 }
--- src/functions/fromResize.ts
+++ src/functions/fromResize.ts
@@ -1,60 +1,64 @@
-import { EMPTY, Observable } from "rxjs";
+import { EMPTY, Observable } from 'rxjs';
 
 export declare interface ResizeObserverSize {
-  readonly inlineSize: number;
-  readonly blockSize: number;
+    readonly inlineSize: number;
+    readonly blockSize: number;
 }
 
 export declare interface ResizeObserverEntry {
-  readonly target: Element;
-  readonly contentRect: DOMRectReadOnly;
-  readonly borderBoxSize: ResizeObserverSize[];
-  readonly contentBoxSize: ResizeObserverSize[];
-  readonly devicePixelContentBoxSize: ResizeObserverSize[];
+    readonly target: Element;
+    readonly contentRect: DOMRectReadOnly;
+    readonly borderBoxSize: ResizeObserverSize[];
+    readonly contentBoxSize: ResizeObserverSize[];
+    readonly devicePixelContentBoxSize: ResizeObserverSize[];
 }
 
 export declare type ResizeObserverCallback = (
-  entries: ResizeObserverEntry[],
-  observer: ResizeObserver
+    entries: ResizeObserverEntry[],
+    observer: ResizeObserver,
 ) => void;
 
 export declare interface ResizeObserverOptions {
-  box: "content-box" | "border-box";
+    box: 'content-box' | 'border-box';
 }
 
 export declare class ResizeObserver {
-  constructor(callback: ResizeObserverCallback);
-  observe(target: Element, options?: ResizeObserverOptions): void;
-  unobserve(target: Element): void;
-  disconnect(): void;
+    constructor(callback: ResizeObserverCallback);
+    observe(target: Element, options?: ResizeObserverOptions): void;
+    unobserve(target: Element): void;
+    disconnect(): void;
 }
 
 export type ResizeObserverEntryWithTypedTarget<
-  T extends Element
+    T extends Element
 > = ResizeObserverEntry & {
-  readonly target: T;
+    readonly target: T;
 };
 
 export function fromResize<T extends Element>(
-  domElement: T,
-  options?: ResizeObserverOptions
+    domElement: T,
+    options?: ResizeObserverOptions,
 ): Observable<ResizeObserverEntryWithTypedTarget<T>> {
-  const globalThat = globalThis || window || self;
-  if (!globalThat) {
-    return EMPTY;
-  }
-  if (typeof (globalThat as any).ResizeObserver !== "function") {
-    return EMPTY;
-  }
-  return new Observable<ResizeObserverEntryWithTypedTarget<T>>((subscriber) => {
-    const elementObserver = new ResizeObserver((entries) => {
-      entries.forEach((entry) => {
-        subscriber.next(entry as ResizeObserverEntryWithTypedTarget<T>);
-      });
-    });
-    elementObserver.observe(domElement, options);
-    return () => {
-      elementObserver.unobserve(domElement);
-    };
-  });
+    const globalThat = globalThis || window || self;
+    if (!globalThat) {
+        return EMPTY;
+    }
+    if (typeof (globalThat as any).ResizeObserver !== 'function') {
+        return EMPTY;
+    }
+    return new Observable<ResizeObserverEntryWithTypedTarget<T>>(
+        (subscriber) => {
+            const elementObserver = new ResizeObserver((entries) => {
+                entries.forEach((entry) => {
+                    subscriber.next(
+                        entry as ResizeObserverEntryWithTypedTarget<T>,
+                    );
+                });
+            });
+            elementObserver.observe(domElement, options);
+            return () => {
+                elementObserver.unobserve(domElement);
+            };
+        },
+    );
 }
